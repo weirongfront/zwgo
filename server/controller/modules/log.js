db47f35996de4bdadc3d5a3f1b14f52335b76794
Mr. Zhao <836600877@qq.com>
2020-06-15 14:47:47 +0800

    修改结构

--- server/controller/modules/log.js
+++ server/controller/modules/log.js
@@ -1,13 +1,9 @@
 const common = require('../../libs/common');
-module.exports = (route,db) => {
-    route.get('/log/list', (req, res) => {
-        const selectUserList = `SELECT * FROM log ORDER BY endtime DESC`;
-        db.query(selectUserList, (err, data) => {
-            if (err) {
-                common.send(req,res,db,500,err.sqlMessage);
-            } else {
-                common.sendSuccess(req,res,db,data);
-            }
-        });
+const logDb = require('../../db/modules/log');
+module.exports = (route,common) => {
+    route.get('/log/list',  (req, res) => {
+        logDb.selectList();
+        common.send(req,res,db,500,err.sqlMessage);
+           
     });
 };
--- server/db/modules/log.js
+++ server/db/modules/log.js
@@ -1,3 +1,17 @@
+const db = require('../index');
+
+const selectList = ()=>{
+    return new Promise((resolve, reject)=>{
+        const selectListSql = `SELECT * FROM log ORDER BY endtime DESC`;
+        db.query(selectListSql, (err, data) => {
+            if(err){
+                reject(err);
+            }else{
+                resolve(data);
+            }
+        });
+    });
+}
 
 
 module.exports = {
